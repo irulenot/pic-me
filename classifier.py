0f6fed3347aaac1fdd599a2281ce3c7c4466d37f
Anthony Bilic <anthony.i.bilic@gmail.com>
2024-06-19 12:04:09 -0700

    class

--- classifier.py
+++ classifier.py
@@ -1,22 +1,24 @@
-from torchvision.io import read_image
-from torchvision.models import resnet18, ResNet18_Weights
+class Classifier:
+    def __init__(self):
+        from torchvision.io import read_image
+        from torchvision.models import resnet18, ResNet18_Weights
+        self.read_image = read_image
+        self.weights = ResNet18_Weights.DEFAULT
+        self.model = resnet18(weights=self.weights)
+        self.model.eval()
 
-img = read_image("meowster.jpeg")
+    def forward(self, img_path):
+        from torchvision.io import read_image
+        img = read_image(img_path)
+        preprocess = self.weights.transforms()
+        batch = preprocess(img).unsqueeze(0)
+        prediction = self.model(batch).squeeze(0).softmax(0)
+        class_id = prediction.argmax().item()
+        score = prediction[class_id].item()
+        category_name = self.weights.meta["categories"][class_id]
+        return category_name, score
 
-# Step 1: Initialize model with the best available weights
-weights = ResNet18_Weights.DEFAULT
-model = resnet18(weights=weights)
-model.eval()
-
-# Step 2: Initialize the inference transforms
-preprocess = weights.transforms()
-
-# Step 3: Apply inference preprocessing transforms
-batch = preprocess(img).unsqueeze(0)
-
-# Step 4: Use the model and print the predicted category
-prediction = model(batch).squeeze(0).softmax(0)
-class_id = prediction.argmax().item()
-score = prediction[class_id].item()
-category_name = weights.meta["categories"][class_id]
+classifier = Classifier()
+img_path = "meowster.jpeg"
+category_name, score = classifier.forward(img_path)
 print(f"{category_name}: {100 * score:.1f}%")
